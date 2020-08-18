31f2c5d8eff46dc239b3dbb76260106cf9d93bbe
Caleb Ledi <caleb.gorrila@gmail.com>
2020-08-18 19:28:16 +0700

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Menu from './components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
 }
-
 export default App;
